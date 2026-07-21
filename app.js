e1e84605db7694dc82e9a674e99e20d93048aaa0
ALANVRE <alanreyna559@gmail.com>
2026-07-20 21:04:38 -0500

    prueba de funcionamiento de auto-deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.json({
     message: '¡Hola! Esta es una aplicación desplegada con JenkinsSSSS y Docker. [v2 - Auto-deploy via SCM polling]',
-    message: '¡Hola! este es un commit de prueba para ver si funciona el auto-deploy',
+    message: '¡Hola! este es un commit de prueba para ver si funciona el auto-deploy via SCM polling',
     version: process.env.APP_VERSION || '2.0.0',
     timestamp: new Date().toISOString()
   });
